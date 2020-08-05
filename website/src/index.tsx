f4ab023f7cf8b2582c1db86e7cf126121d5e1820
najm09 <fnajm09@gmail.com>
2020-08-05 14:45:56 +0530

    setting up Header

--- website/src/index.tsx
+++ website/src/index.tsx
@@ -5,14 +5,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import App from './components/App'
 import Login from './components/views/Admin/Login'
 import Signup from './components/views/Admin/Signup'
-
+import Header from './components/views/User/Header'
 ReactDOM.render(
 
   <BrowserRouter>
     <Switch>
-      <Route exact path='/' render={()=><App/>}/>
+      <Route exact path='/' render={()=><Header ><App/></Header>}/>
       <Route exact path='/Login' render={()=><Login/>}/>
-<Route exact path='/Signup' render={()=><Signup/>}/>
+<Route exact path='/Signup' render={()=><Header><Signup/></Header>}/>
     </Switch>
   </BrowserRouter>,
   document.getElementById('root')
