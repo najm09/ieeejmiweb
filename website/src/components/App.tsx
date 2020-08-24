5f14ce5dabaa259f9b122997cbda8483c3380ed3
najm09 <fnajm09@gmail.com>
2020-08-24 19:01:25 +0530

    upgrading into functional component

--- website/src/components/App.tsx
+++ website/src/components/App.tsx
@@ -1,39 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
 import fire from './config/Fire'
 import Login from './views/Admin/Login'
 import Dashboard from './views/Admin/Dashboard'
 
-class App extends Component {
-  state = { user: {} };
+export default function App() {
 
-  componentDidMount() {
-    this.authListener();
-  }
+  const [user, setUser] = React.useState(null);
 
-  // checking for authenticated user
-
-  authListener() {
-    fire.auth().onAuthStateChanged((user) => {
-      if (user) {
-        this.setState({ user });
-        // session start
+  fire.auth().onAuthStateChanged((user) => {
+    if (user) {
+      setUser(user);
+      // session start
+    }
+    else {
+      setUser(null);
+    }
+  })
+  
+  return (
+    <div>
+      {
+        user ? <Dashboard /> : <Login />
       }
-      else {
-        this.setState({ user: null })
-      }
-    })
-  }
+    </div>
+  )
 
-  render() {
-
-    return (
-      <div>
-        {
-          this.state.user ? <Dashboard /> : <Login />
-        }
-      </div>
-    )
-  }
 }
-
-export default App
