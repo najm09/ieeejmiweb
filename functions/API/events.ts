648dcc5eef85918a6dc6e96dc6719e938d0443e5
amilsaleem <31254366+amilsaleem@users.noreply.github.com>
2020-08-27 22:19:52 +0530

    Email Templating

---
+++ functions/API/events.ts
@@ -0,0 +1,90 @@
+import {db} from '../Util/admin'
+import {Request, Response} from 'express'
+
+interface Event {
+    title: string,
+    username: string,
+    body: string,
+    invitations: [string],
+    time: Date,
+    createdAt: Date
+}
+
+export const getAllEvents = (request : Request, response : Response) => {
+    db
+        .collection('events')
+        .where('username', '==', request.user.username)
+        .orderBy('createdAt', 'desc')
+        .get()
+        .then(data => {
+            return response.json({data})
+        })
+        .catch(err => {
+            console.error(err)
+            return response.status(500).json({error : err.code})
+        })
+}
+
+export const getEvent = (request : any, response : any) => {
+    db
+        .doc(`/events/${request.params.todoId}`)
+        .get()
+        .then(doc => {
+            if(!doc.exists){
+                return response.status(404).json({
+                    error: 'Events id does\'nt exist'
+                })
+            }
+            if(doc.data().username !== request.user.username){
+                return response.status(403).json("UnAutharized")
+            }
+            return response.json(doc)
+        })
+        .catch(err => {
+            console.error(err)
+            return response.status(500).json({error : err.code})
+        })
+}
+export const createEvent = (request : any, response : any) => {
+    const newEvent : Event = {
+        title: request.body.title,
+        username: request.body.username,
+        body: request.body.body,
+        invitations: request.body.list,
+        time : request.body.time,
+        createdAt: new Date()
+    }
+
+    db
+        .collection('events')
+        .add(newEvent)
+        .then(doc => {
+            return response.json(doc)
+        })
+        .catch(err => {
+            console.error(err)
+            response.status(500).json({error : 'Something went wrong'})
+        })
+}
+
+export const editEvent = (request : any, response : any) => {
+    if(request.body.eventId || request.body.createdAt){
+        response.status(403).json({message: 'Not allowed to edit'})
+    }
+    const document = db.collection('events').doc(`${request.params.eventId}`)
+    document.update(request.body)
+    .then(doc => {
+        response.json({message : "Updated Succesfully"})
+    })
+    .catch(err => {
+        if(err.code === 5){
+            response.status(500).json({
+                message: "Not Found"
+            })
+        }
+        console.error(err)
+        response.status(500).json({
+            error: err.code
+        })
+    })
+}
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from 'firebase-functions';
+<<<<<<< Updated upstream
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,3 +7,20 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+=======
+import * as express from 'express'
+import auth from '../Util/auth'
+import * as userAuth from '../API/user'
+import * as events from '../API/events'
+
+
+const app = express()
+
+app.post('/login', userAuth.loginUser)
+app.post('/signUp', userAuth.signUpUser)
+app.post('/updateProfile', auth, userAuth.updateUserDetails)
+app.post('/makeAdmin', auth, userAuth.makeAdmin)
+app.get('/user', auth, userAuth.getUserDetails)
+
+export const api = functions.https.onRequest(app)
+>>>>>>> Stashed changes
